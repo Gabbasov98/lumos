38447f30f4f94351b1e9ede7c3db0037c574e078
Marsel Gabbasov <marsel13_98@mail.ru>
2022-01-16 17:51:56 +0300

    2-3 pages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,133 @@
+function courseSlider() {
+    var swiper = new Swiper('.course-slider1 .swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.course-slider1 .swiper-button-next',
+            prevEl: '.course-slider1 .swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.05,
+                spaceBetween: 16
+            },
+            400: {
+                slidesPerView: 1.3,
+            },
+            460: {
+                slidesPerView: 1.5,
+            },
+            500: {
+                slidesPerView: 1.7,
+            },
+            690: {
+                slidesPerView: 2,
+                spaceBetween: 24
+            },
+            950: {
+                slidesPerView: 2.5,
+
+            },
+            1270: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+        }
+    })
+}
+
+function courseSlider2() {
+    var swiper = new Swiper('.course-slider2 .swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.course-slider2 .swiper-button-next',
+            prevEl: '.course-slider2 .swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.05,
+                spaceBetween: 16
+            },
+            400: {
+                slidesPerView: 1.3,
+            },
+            460: {
+                slidesPerView: 1.5,
+            },
+            500: {
+                slidesPerView: 1.7,
+            },
+            690: {
+                slidesPerView: 2,
+                spaceBetween: 24
+            },
+            950: {
+                slidesPerView: 2.5,
+
+            },
+            1270: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+        }
+    })
+}
+
+function courseDetail() {
+    var swiper = new Swiper('.course-detail .swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: '.course-detail .swiper-button-next',
+            prevEl: '.course-detail .swiper-button-prev',
+        },
+        pagination: {
+            el: '.course-detail .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    })
+}
+
+function variantSlider() {
+    var swiper = new Swiper('.course-variants .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 24,
+    })
+}
+
+function feedbackSlider() {
+    var swiper = new Swiper('.course-feedback .swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 24,
+        navigation: {
+            nextEl: '.course-feedback .swiper-button-next',
+            prevEl: '.course-feedback .swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 24
+            },
+
+            1024: {
+                slidesPerView: 2,
+            }
+        }
+    })
+}
+
+
 document.addEventListener('DOMContentLoaded', function() { // Аналог $(document).ready(function(){
+    courseSlider()
+    courseSlider2()
+    courseDetail()
+    feedbackSlider()
+    if (window.innerWidth > 1024) {
+        variantSlider()
+    }
 
     document.querySelector(".header__burger").onclick = function() {
         this.classList.toggle('header__burger--active');
@@ -13,5 +142,22 @@ document.addEventListener('DOMContentLoaded', function() { // Аналог $(doc
     document.querySelector(".header__search").onclick = function() {
         this.classList.add('header__search--active');
     }
+
+
+
+    document.querySelectorAll(".filter-tablet__item").forEach(el => {
+        el.onclick = function() {
+            let path = this.getAttribute('data-filter');
+            console.log(path)
+            document.querySelector(`.filter__items[data-filter="${path}"]`).classList.add("filter__items--active")
+            document.querySelector(".filter").classList.add("filter--active")
+        }
+    });
+
+
+
+
+
+
 
 });
